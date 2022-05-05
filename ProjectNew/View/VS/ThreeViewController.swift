b591dab8e2e84ad5c64406dae45557a4fd471baa
dimov-danya1999 <100965240+dimov-danya1999@users.noreply.github.com>
2022-05-05 22:39:48 +0300

    SportProject

---
+++ ProjectNew/View/VS/ThreeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ThreeViewController.swift
+//  ProjectNew
+//
+//  Created by mac on 05.05.2022.
+//
+
+import UIKit
+
+class ThreeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor(named: "mySert")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
